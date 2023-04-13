386da4e8365c06b1cdb76d5ed5057386251db3e2
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-13 23:22:49 +1000

    smooth typing works now

--- src/ui/src/pages/_document.tsx
+++ src/ui/src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <title>Imaginaition</title>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
