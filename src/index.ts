bd72ba20f20b1e903bb21071510415278509950f
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-16 08:57:48 +1000

    rebrand

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,16 @@
 import CutApp from "./apps/cut";
 import { emphasis, error, title } from "./utils/gradients";
 
-let version = "1.0.0";
+let version = "1.0.1";
 
 async function main() {
-  console.log(title(`Imaginaition ${version}`));
-  if (process.argv.includes("cut")) {
-    console.log(emphasis("Running `cut`"));
-    let index = process.argv.indexOf("cut");
-    if (index + 1 == process.argv.length) {
+  console.log(title(`Autocut ${version}`));
+    if (!process.argv[2]) {
       console.log(error("No file path given."));
       return;
     }
-    let inputFilePath = process.argv[index + 1];
+    let inputFilePath = process.argv[2];
     CutApp(inputFilePath);
-  }
 }
 
 main();
