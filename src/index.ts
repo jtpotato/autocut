282f034b903fc9ccbd56a476d4e70a454526f721
Joel <58995538+jtpotato@users.noreply.github.com>
2023-04-17 13:27:46 +1000

    fix some date-related bugs

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,41 @@
 #! /usr/bin/env node
 
-import fs from 'fs/promises';
-import { exec } from 'child_process';
+import fs from "fs/promises";
+import { exec } from "child_process";
 import { debug, emphasis, error, title } from "./utils/gradients.js";
 
-console.log(title("Autocut 2.0.0 🚀"))
+console.log(title("Autocut 2.0.1 🚀"));
 
 // Create concat-list.txt with filenames of all .MOV files in the current directory
 async function main() {
   try {
-    const files = await fs.readdir('.');
-    const movFiles = files.filter((file) => file.toLowerCase().endsWith('.mov'));
-    const concatList = movFiles.map((file) => `file ${file}`).join('\n');
-    await fs.writeFile('concat-list.txt', concatList);
+    const files = await fs.readdir(".");
+    const movFiles = files.filter((file) =>
+      file.toLowerCase().endsWith(".mov")
+    );
+    const concatList = movFiles.map((file) => `file ${file}`).join("\n");
+    await fs.writeFile("concat-list.txt", concatList);
 
     // Use ffmpeg to concatenate the videos and output merged.mp4
-    console.log(emphasis("Concatenating videos 🪄"))
-    await execPromise('ffmpeg -f concat -safe 0 -i concat-list.txt -c:v copy merged.mp4');
+    console.log(emphasis("Concatenating videos 🪄"));
+    const outputFileName = `${new Date()
+      .toLocaleString()
+      .replaceAll(/[^0-9]+/g, "-")}.mp4`;
+    await execPromise(
+      `ffmpeg -f concat -safe 0 -i concat-list.txt -c:v copy ${outputFileName}`
+    );
 
     // Print the list of concatenated videos
-    console.log(emphasis('Concatenated ✨'));
-    console.log(debug(movFiles.join('\n')));
+    console.log(emphasis("Concatenated ✨"));
+    console.log(debug(movFiles.join("\n")));
 
     // Use auto-editor to apply editing and export to final-cut-pro format
-    console.log(emphasis('Applying edits 🎬'));
-    await execPromise('auto-editor merged.mp4 --export final-cut-pro --margin 0.1sec');
+    console.log(emphasis("Applying edits 🎬"));
+    await execPromise(`auto-editor  --export final-cut-pro --margin 0.1sec`);
 
     // Clean up concat-list.txt
-    await fs.unlink('concat-list.txt');
-    console.log(title('All done! 🎉'));
+    await fs.unlink("concat-list.txt");
+    console.log(title("All done! 🎉"));
   } catch (err) {
     console.error(error(err.toString()));
   }
