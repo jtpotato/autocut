00ff02f74b254f90848a6d3a318173d6b22e4c75
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-27 16:17:19 +1000

    alright lets make this from scratch now

--- src/utils/gradients.ts
+++
@@ -1,8 +0,0 @@
-import gradient from "gradient-string";
-
-let error = gradient("#ff0000", "#ff0090")
-let debug = gradient("#e3b8e0", "#f2e4c9")
-let emphasis = gradient("#3077D9", "#0BBAD5")
-let title = gradient("#f7cfb4", "#e26fb6")
-
-export { error, debug, emphasis, title }
